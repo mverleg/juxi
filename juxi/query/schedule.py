5ba95fc9b0edb17f9f05afe19239d6e68f53f777
Mark <mverleg.noreply@gmail.com>
2024-11-27 00:08:59 +0100

    Query notes, cant get django to drop group by

--- juxi/query/schedule.py
+++ juxi/query/schedule.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 from typing import Optional, List
 
 from django.db import reset_queries, connection
-from django.db.models import Min, Max
+from django.db.models import Min, Max, OuterRef, Subquery
 
 from juxi.models import TaskRun, TaskSeries
 
@@ -16,25 +16,49 @@ class TaskOverview:
 
 def task_overview() -> List[TaskOverview]:
 
-    # SELECT *
-    # FROM juxi_taskrun AS outr
-    # WHERE outr.end_at = (
-    #     SELECT MAX(innr.end_at)
-    #     FROM juxi_taskrun AS innr
-    #     WHERE outr.series_id == innr.series_id
-    # );
+    # The query we want is this; Django inserts a useless GROUP BY
+    # but hopefully the database sees that it's useless and optimizes it
+    #   SELECT *
+    #   FROM juxi_taskrun AS outr
+    #   WHERE outr.end_at = (
+    #       SELECT MAX(innr.end_at)
+    #       FROM juxi_taskrun AS innr
+    #       WHERE outr.series_id == innr.series_id
+    #   );
 
-    reset_queries()
-    series = TaskRun.objects \
-        .values('series_id') \
-        .annotate(series=Min('series_id'), last_run=Max('start_at'))
-    print(series)
-    print(
-        TaskRun.objects
-        .values('series_id')
-        .annotate(latest_start_at=Max('start_at'))
-        .all())
+    reset_queries()  #TODO @mark: TEMPORARY! REMOVE THIS!
 
-    print('\n'.join(q['sql'] for q in connection.queries))
+    subquery = TaskRun.objects.filter(
+        series_id=OuterRef('series_id')
+    ).values('series_id').annotate(
+        max_end_at=Max('end_at')
+    ).values('max_end_at')
+
+    queryset = TaskRun.objects.filter(
+        end_at=Subquery(subquery)
+    )
+
+    print(queryset.query)
+    print(queryset.all())
+
+
+    # djq = TaskRun.objects.filter(end_at=TaskRun.objects.values(latest=Max('end_at')).filter(series_id=OuterRef('series_id')))
+    # djq = TaskRun.objects.values('series_id').annotate(latest=Max('end_at'))
+    # djq = TaskRun.objects.filter(end_at=Subquery(TaskRun.objects.filter(series_id=OuterRef('series_id')).aggregate(Max('end_at'))))
+    # print(djq.query)
+    # res = djq.all()
+    # print('\n'.join(q['sql'] for q in connection.queries))
+    # print(res)
+
+    # series = TaskRun.objects \
+    #     .values('series_id') \
+    #     .annotate(series=Min('series_id'), last_run=Max('start_at'))
+    # print(series)
+    # print(
+    #     TaskRun.objects
+    #     .values('series_id')
+    #     .annotate(latest_start_at=Max('start_at'))
+    #     .all())
+
 
 

c0206feac8821b896ac1849d907eba3501124485
Mark <mverleg.noreply@gmail.com>
2024-11-26 22:16:31 +0100

    Style form errors (red)

--- juxi/views/home.py
+++ juxi/views/home.py
@@ -7,8 +7,5 @@ def home(request):
     if not request.user.is_authenticated:
         return redirect(f"{reverse('login')}?next={request.get_full_path()}")
         #TODO @mark: make this default everywhere
-    messages.info(request, f"Welcome, {request.user.username}! (home)")
-    messages.warning(request, f"Welcome, {request.user.username}! (home)")
-    messages.error(request, f"Welcome, {request.user.username}! (home)")
     return render(request, 'home.html')
 
--- juxi/views/util.py
+++ juxi/views/util.py
@@ -1,4 +1,4 @@
 
-INPUT_CLASSES = 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-fuchsia-400 focus:border-fuchsia-400 block w-full p-2.5'
-ERROR_CLASSES = 'bg-fuchsia-50 border-y-4 my-6 border-fuchsia-600 rounded-lg'
+INPUT_CLASSES = 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-400 focus:border-red-400 block w-full p-2.5'
+ERROR_CLASSES = 'bg-red-50 border-y-4 my-4 py-2 border-red-600 rounded-lg '
 

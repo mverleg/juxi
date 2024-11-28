d2667764abdc9204f424f2492c5d1035d24f1d9c
Mark <mverleg.noreply@gmail.com>
2024-11-28 19:03:37 +0100

    Add timezone to cookie

---
+++ juxi/static/juxi.js
@@ -0,0 +1,8 @@
+
+// Timezone settings
+const tz_cookie_str = 'juxi.timezone=' + Intl.DateTimeFormat().resolvedOptions().timeZone;
+if (!document.cookie || !document.cookie.includes(tz_cookie_str)) {
+    console.log('setting timezone cookie:', tz_cookie_str)
+    document.cookie = tz_cookie_str;
+}
+

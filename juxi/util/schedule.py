900f9caff7861e69d80e6908da11dba17c56d08e
Mark <mverleg.noreply@gmail.com>
2024-11-28 22:42:46 +0100

    Date shifting issues

--- juxi/util/schedule.py
+++ juxi/util/schedule.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 
+from dateutil.relativedelta import relativedelta
 from django.utils import timezone
 
 from juxi.data.schedule import MONTH, WEEK, DAY, HOUR, MINUTE
@@ -13,16 +14,42 @@ def next_occurrence(now: datetime, reference: datetime, time_unit: str, every_nt
 
     next = reference.replace(second=0, microsecond=0)
     if time_unit == MONTH:
-        month_diff = (reference.year * 12 + reference.month) - (now.year * 12 + now.month)
-        next.replace(year=next.year + month_diff // 12, month=next.month + month_diff % 12)
-        # next.replace(year=now.year, month=now.month)
-        # if next < now:
-        #     next.replace(month=now.month + 1)
-        #TODO @mark: this only works if every_nth is 1
-        return next
+        return _next_occurrence_month(now, next, every_nth)
     if time_unit in {WEEK, DAY}:
+        print(f'TODO: {time_unit}')
         return now  #TODO @mark:
     if time_unit in {HOUR, MINUTE}:
+        print(f'TODO: {time_unit}')
         return now  #TODO @mark:
     raise AssertionError(f"unknown time unit {time_unit}")
 
+
+def _next_occurrence_month(now: datetime, next: datetime, every_nth: int):
+    print('')
+    print('reference', next)
+    month_diff_pure = (now.year * 12 + now.month) - (next.year * 12 + next.month)
+    month_diff_steps = (month_diff_pure // every_nth) * every_nth
+    next = _add_months(next, month_diff_steps)
+    print('month_diff_pure', month_diff_steps, month_diff_steps)
+    print('next', next)
+    while next > now:
+        print(f'next = _add_months({next}, -{every_nth})')
+        next = _add_months(next, -every_nth)
+        print('subtract another', every_nth, 'next', next)
+    else:
+        print('no extra - shift')
+    while next < now:
+        next = _add_months(next, every_nth)
+        print('add another', every_nth, 'next', next)
+    else:
+        print('no extra + shift')
+    # next.replace(year=now.year, month=now.month)
+    # if next < now:
+    #     next.replace(month=now.month + 1)
+    #TODO @mark: this only works if every_nth is 1
+    return next
+
+
+def _add_months(dt: datetime, add_months: int) -> datetime:
+    print('subtract', add_months, 'from', dt)
+    return dt + relativedelta(month=add_months)
--- tests/util/test_schedule.py
+++ tests/util/test_schedule.py
@@ -5,23 +5,31 @@ import pytz
 from juxi.data.schedule import MONTH
 from juxi.util.schedule import next_occurrence
 
+
 TZ = pytz.timezone("Europe/Amsterdam")
 
 def test_monthly_tomorrow():
     event = next_occurrence(now=dt(2024, 11, 28, 12), reference=dt(2024, 11, 29, 12), time_unit=MONTH, every_nth=3)
     assert event == dt(2024, 11, 29, 12)
+    event = next_occurrence(now=dt(2025, 1, 31, 23), reference=dt(2025, 1, 31, 22), time_unit=MONTH, every_nth=1)
+    assert event == dt(2025, 2, 28, 22)
 
 def test_monthly_last_year():
-    event = next_occurrence(now=dt(2024, 11, 14, 12), reference=dt(2023, 11 - 6, 13, 0), time_unit=MONTH, every_nth=2)
+    event = next_occurrence(now=dt(2024, 11, 14, 12), reference=dt(2023, 6, 13, 0), time_unit=MONTH, every_nth=2)
+    assert event == dt(2024, 12, 13, 0)
+    event = next_occurrence(now=dt(2024, 11, 14, 12), reference=dt(2023, 5, 13, 0), time_unit=MONTH, every_nth=2)
     assert event == dt(2025, 1, 13, 0)
 
 def test_monthly_future_year():
     event = next_occurrence(now=dt(2024, 11, 28, 12), reference=dt(2026, 6, 15, 10), time_unit=MONTH, every_nth=3)
     assert event == dt(2024, 12, 15, 10)
+    event = next_occurrence(now=dt(2024, 11, 28, 12), reference=dt(2028, 5, 28, 2), time_unit=MONTH, every_nth=4)
+    assert event == dt(2025, 1, 28, 2)
 
 
 #TODO @mark: test that seconds/millis can removed
 
 def dt(year, month, day, hour):
     return datetime(year, month, day, hour, 0, 0, 0, tzinfo=TZ)
+
 

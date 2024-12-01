2bdb21f39fb16aaf0d55c90c02ae55266b307b49
Mark <mverleg.noreply@gmail.com>
2024-12-01 16:17:30 +0100

    Test special cases around end-of-month

--- tests/util/test_schedule_monthly.py
+++ tests/util/test_schedule_monthly.py
@@ -23,10 +23,6 @@ def test_intrayear_forward_samemonth_shorter():
     event = next_occurrence(now=dt(2024, 11, 15, 1), reference=dt(2024, 7, 31, 2), time_unit=MONTH, every_nth=4)
     assert event == dt(2024, 11, 30, 2)
 
-def weird_case_to_check():  #TODO @mark:
-    event = next_occurrence(now=dt(2024, 11, 1, 2), reference=dt(2023, 2, 1, 1), time_unit=MONTH, every_nth=3)
-    assert event == dt(2024, 11, 1, 1)
-
 def test_interyear_forward_nextmonth():
     event = next_occurrence(now=dt(2024, 10, 1, 2), reference=dt(2023, 2, 1, 1), time_unit=MONTH, every_nth=3)
     assert event == dt(2024, 11, 1, 1)
@@ -71,18 +67,31 @@ def test_intrayear_backard_nextmonth():
 def test_intrayear_backard_nextmonth_shorter():
     assert False
 
-def test_intrayear_nochange():
-    assert False
+def test_nochange_sameday():
+    event = next_occurrence(now=dt(2024, 7, 15, 2), reference=dt(2024, 7, 15, 3), time_unit=MONTH, every_nth=3)
+    assert event == dt(2024, 7, 15, 3)
 
-def test_intrayear_nochange_shorter():
-    assert False
+def test_nochange_endofmonth():
+    event = next_occurrence(now=dt(2024, 7, 1, 23), reference=dt(2024, 7, 31, 1), time_unit=MONTH, every_nth=3)
+    assert event == dt(2024, 7, 31, 1)
 
-def test_pick_shorter_months_when_starting_from_end_of_month():
-    #TODO @mark: two cases here:
-    # day of month capping should never go before `now`
-    # starting on 31st should not skip half the months
-    event = next_occurrence(now=dt(2024, 9, 30, 1), reference=dt(2023, 1, 31, 2), time_unit=MONTH, every_nth=3)
-    assert event == dt(2024, 9, 30, 1)
+def test_should_not_skip_short_months():
+    event = next_occurrence(now=dt(2024, 5, 31, 10), reference=dt(2024, 5, 31, 9), time_unit=MONTH, every_nth=1)
+    assert event == dt(2024, 6, 30, 9)
+
+def test_day_of_month_cap_should_not_move_before_now_1():
+    event = next_occurrence(now=dt(2024, 5, 31, 14), reference=dt(2023, 5, 31, 12), time_unit=MONTH, every_nth=1)
+    assert event != dt(2024, 5, 31, 12)
+    assert event == dt(2026, 6, 30, 12)
+
+def test_day_of_month_cap_should_not_move_before_now_2():
+    event = next_occurrence(now=dt(2024, 2, 28, 23), reference=dt(2023, 12, 31, 1), time_unit=MONTH, every_nth=2)
+    assert event != dt(2024, 2, 28, 1)
+    assert event == dt(2024, 4, 30, 1)
+
+def weird_case_to_check():  #TODO @mark:
+    event = next_occurrence(now=dt(2024, 11, 1, 2), reference=dt(2023, 2, 1, 1), time_unit=MONTH, every_nth=3)
+    assert event == dt(2024, 11, 1, 1)
 
 def test_feb_nonleap_year():
     event = next_occurrence(now=dt(2023, 2, 28, 1), reference=dt(2023, 1, 31, 2), time_unit=MONTH, every_nth=1)
